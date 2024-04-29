872c340405c3547d96d78b83273c17da4824ad11
Paola-Santanna <paolinhasj@gmail.com>
2024-04-29 12:14:54 -0300

    Add project files.

---
+++ POO - Lista Telefônica Pessoas/Pessoas.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Pessoas
+{
+    public string Nome;
+    public int Idade;
+    public string Sobrenome;
+    public string Telefone;
+    public string Celular;
+    public string Email;
+
+
+    public Pessoas() //Método construtor
+    {
+
+    }
+
+    public void DefinirNome(string nome)
+    {
+        this.Nome = nome;
+    }
+
+    public void DefinirSobrenome(string sobrenome)
+    {
+        this.Sobrenome = sobrenome;
+    }
+
+    public void DefinirIdade(int idade)
+    {
+        this.Idade = idade;
+    }
+
+    public void DefinirTelefone(string telefone)
+    {
+        this.Telefone = telefone;
+    }
+
+    public void DefinirCelular(string celular)
+    {
+        this.Celular = celular;
+    }
+
+    public void DefinirEmail(string email)
+    {
+        this.Email = email;
+    }
+
+    public void MostrarDados()
+    {
+        Console.WriteLine($"Nome: {Nome} {Sobrenome}", this.Nome, this.Sobrenome);
+        Console.WriteLine($"Idade: {Idade}", this.Idade);
+        Console.WriteLine($"Telefone: {Telefone}", this.Telefone);
+        Console.WriteLine($"Celular: {Celular}", this.Celular);
+        Console.WriteLine($"Email: {Email}", this.Email);
+    }
+}
+
---
+++ POO - Lista Telefônica Pessoas/Program.cs
@@ -0,0 +1,58 @@
+﻿// Lista telefônica que cadastre 3 pessoas
+string nome, sobrenome, telefone, celular, email;
+int idade;
+
+Pessoas p1 = new Pessoas();
+Pessoas p2 = new Pessoas();
+Pessoas p3 = new Pessoas();
+
+Pessoas CriarPessoa()
+{
+    Pessoas p = new Pessoas();
+
+    Console.Write("Digite o seu nome: ");
+    nome = Console.ReadLine();
+    p.DefinirNome(nome);
+
+    Console.Write("Digite o seu sobrenome: ");
+    sobrenome = Console.ReadLine();
+    p.DefinirSobrenome(sobrenome);
+
+    Console.Write("Digite a sua idade: ");
+    idade = int.Parse(Console.ReadLine());
+    p.DefinirIdade(idade);
+
+    Console.Write("Digite o seu telefone: ");
+    telefone = Console.ReadLine();
+    p.DefinirTelefone(telefone);
+
+    Console.Write("Digite o seu celular: ");
+    celular = Console.ReadLine();
+    p.DefinirCelular(celular);
+
+    Console.Write("Digite o seu email: ");
+    email = Console.ReadLine();
+    p.DefinirEmail(email);
+
+    return p;
+}
+
+#region p1
+p1 = CriarPessoa();
+Console.WriteLine();
+p1.MostrarDados();
+#endregion
+
+Console.WriteLine();
+#region p2
+p2 = CriarPessoa();
+Console.WriteLine();
+p2.MostrarDados();
+#endregion
+
+Console.WriteLine();
+#region p3
+p3 = CriarPessoa();
+Console.WriteLine();
+p3.MostrarDados();
+#endregion
